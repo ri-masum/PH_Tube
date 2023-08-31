0febeb5c76b90961e9236cc3da7e8f28d8c0fc96
masumahemd <rimasum01724@gmail.com>
2023-08-31 22:33:01 +0600

    top button fetch from api done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('conencted');
+const loadProduct=async ()=>{
+    const res=await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data=await  res.json();
+    const product=data.data;
+    displayProduct(product);
+}
+
+const displayProduct=(products=>{
+    console.log(products);
+
+    const productContainer=document.getElementById('product-nav');
+
+    products.forEach((product) => {
+        const productNav=document.createElement('div');
+        productNav.classList=``
+        productNav.innerHTML=`
+        <button class="btn btn-active rounded-lg">${product.category}</button>
+        `
+        productContainer.appendChild(productNav);
+
+    });
+})
+
+loadProduct();
